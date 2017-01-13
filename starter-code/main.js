5638f621dad9a4884605964272192481976bcf0a
Ben Currie <123THC@Bens-MacBook-Pro.local>
2017-01-13 10:33:01 +0000

    added alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = "King";
 var cardTwo = "King";
 var cardThree = "Queen";
 var cardFour = "Queen";
+
+if (cardOne === cardTwo || cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry try again!")
+}
