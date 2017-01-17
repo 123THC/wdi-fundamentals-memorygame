5bb5aea7676442573edcb58b7c23a9ac4ef4333b
Ben Currie <123THC@Bens-MacBook-Pro.local>
2017-01-17 12:34:47 +0000

    created divs usng javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,25 @@ var cardTwo = "King";
 var cardThree = "Queen";
 var cardFour = "Queen";
 
-if (cardOne === cardTwo || cardThree === cardFour) {
-	alert("You found a match!");
-} else {
-	alert("Sorry try again!")
-}
+//if (cardOne === cardTwo || cardThree === cardFour) {
+//	alert("You found a match!");
+//} else {
+//	alert("Sorry try again!")
+//}
+
+var gameBoard = document.getElementById('game-board');
+
+    var createCards = function(){
+        
+        for (var i = 0; i <= 3; i++) {
+          var playingCards = document.createElement('div');
+            playingCards.className = 'card';
+            document.querySelector('div').appendChild(playingCards);
+            }
+        }
+createCards();
+
+
+
+
+
