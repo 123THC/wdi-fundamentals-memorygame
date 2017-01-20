744382a6f9fdd09d13c2cb8c5c64e12bed15fc1f
Ben Currie <123THC@Bens-MacBook-Pro.local>
2017-01-20 13:51:05 +0000

    added more functions but still has few bugs to fix

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,28 +1,70 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
+//var cardOne = "King";
+//var cardTwo = "King";
+//var cardThree = "Queen";
+//var cardFour = "Queen";
 
-//if (cardOne === cardTwo || cardThree === cardFour) {
-//	alert("You found a match!");
-//} else {
-//	alert("Sorry try again!")
-//}
+var cards = ['king', 'king', 'queen', 'queen', 'jack', 'jack', 'ten', 'ten'];
+var cardsInPlay = [];
 
 var gameBoard = document.getElementById('game-board');
 
     var createCards = function(){
         
-        for (var i = 0; i <= 3; i++) {
+        for (var i = 0; i < cards.length; i++) {
           var playingCards = document.createElement('div');
-            playingCards.className = 'card';
+             playingCards.className = 'card';
             document.querySelector('div').appendChild(playingCards);
+            playingCards.setAttribute('data-card', cards[i]);
+            playingCards.addEventListener('click', isTwoCards);
+            playingCards.innerHTML = '<img src="frontcard.jpg" alt="Card"/>'
             }
-        }
-createCards();
+         }
+ createCards();
 
+function isTwoCards() {
+	var dataCard = this.getAttribute('data-card');
+	cardsInPlay.push(dataCard);
+
+	if (dataCard === "king"){
+		this.innerHTML = '<img src="theking.png" alt="King"/>';
+	} else if (dataCard === "queen"){
+		this.innerHTML = '<img src="queen.png" alt="Queen"/>';
+	} else if (dataCard === "jack"){
+		this.innerHTML = '<img src="jack.png" alt="Jack"/>';
+	} else if (dataCard === "ten"){
+		this.innerHTML = '<img src="ten.jpg" alt="Ten"/>';
+	}
+	
+	if (cardsInPlay.length === 2) {
+		isMatch();
+		cardsInPlay = [];
+	} 
+}
+
+// I need to create a time delay for the alert and also for the reset
+//setTimeout(reset, 3000);
+
+function isMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+} else {
+	reset();
+	alert("Sorry, try again!");
+}
+}
+
+// I need to edit this function so it doesn't flip over all the cards when one pair doesn't match
+function reset() {
+	for (i = 0; i < cards.length; i++){
+		if (document.getElementsByClassName('card'))[i]
+			document.getElementsByClassName('card')[i].innerHTML = '<img src="frontcard.jpg" alt="Card"/>';
+		}
+	}
+
+
+	
 
 
 
